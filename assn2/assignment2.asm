68bc360a4d3b8a0762ffe6cca1c1459723a731a2
Kyrithdagon <150574824+Kyrithdagon@users.noreply.github.com>
2023-11-11 22:27:35 -0800

    Update assignment2.asm

--- assn2/assignment2.asm
+++ assn2/assignment2.asm
@@ -1,17 +1,3 @@
-;=========================================================================
-; Name & Email must be EXACTLY as in Gradescope roster!
-; Name: Sharon Lee
-; Email: slee900@ucr.edu
-; 
-; Assignment name: Assignment 2
-; Lab section: 021
-; TA: Westin Montano, Nick Santini
-; 
-; I hereby certify that I have not received assistance on this assignment,
-; or used code, from ANY outside source other than the instruction team
-; (apart from what was provided in the starter file).
-;
-;=========================================================================
 
 .ORIG x3000			; Program begins here
 ;-------------

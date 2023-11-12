286129c1a2cf82d7a2535c1c8a5812836d12ad83
Kyrithdagon <150574824+Kyrithdagon@users.noreply.github.com>
2023-11-11 22:16:02 -0800

    Update lab4_ex3.asm

--- lab4/lab4_ex3.asm
+++ lab4/lab4_ex3.asm
@@ -1,12 +1,3 @@
-;=================================================
-; Name: Sharon Lee
-; Email: slee900@ucr.edu
-; 
-; Lab: lab 4, ex 3
-; Lab section: 021
-; TA: Westin Montano, Nick Santini
-; 
-;=================================================
 
 .ORIG x3000
 
